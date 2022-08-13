3353e27c42a04a5c4fd5c9f1dc0e846d21da887b
Dasuki <daniel.i.kristiansson@gmail.com>
2022-08-14 02:02:23 +0300

    added mod effects and enchantments

--- src/main/java/dasuki/goblinite/Goblinite.java
+++ src/main/java/dasuki/goblinite/Goblinite.java
@@ -1,6 +1,8 @@
 package dasuki.goblinite;
 
 import dasuki.goblinite.block.ModBlocks;
+import dasuki.goblinite.effects.ModEffects;
+import dasuki.goblinite.enchantment.ModEnchantments;
 import dasuki.goblinite.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
@@ -11,10 +13,13 @@ public class Goblinite implements ModInitializer {
 	public static final String MOD_ID = "goblinite";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-
-	@Override
+    @Override
 	public void onInitialize() {
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
+
+		ModEffects.registerModEffects();
+		ModEnchantments.registerModEnchantments();
+		
 	}
 }
