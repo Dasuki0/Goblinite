8c05a9c6ac973a00acaf48a143edcb4d517a49fd
Dasuki <daniel.i.kristiansson@gmail.com>
2022-08-14 02:01:42 +0300

    created file

---
+++ src/main/java/dasuki/goblinite/item/custom/ModScytheItem.java
@@ -0,0 +1,18 @@
+package dasuki.goblinite.item.custom;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
+
+public class ModScytheItem extends SwordItem {
+    public ModScytheItem(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
+        super(toolMaterial, attackDamage, attackSpeed, settings);
+    }
+
+    @Override
+    public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+
+        return super.postHit(stack, target, attacker);
+    }
+}
